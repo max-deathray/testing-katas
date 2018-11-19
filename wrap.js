0c3d83dcbacbfda7050d7ca18eba39c872aec1c8
McRae Petrey <mpetrey@gmail.com>
2018-11-19 12:02:09 -0500

    pushing back up to "the CLOUD!"

--- wrap.js
+++ wrap.js
@@ -1,15 +1,20 @@
 const wrap = (line, maxLen) => {
-  let output = "";
+  let output = '';
   // let lineStart = 0;
 
   for (let i = 0; i < line.length; i++) {
     if (i === maxLen - 1) {
-      if (line[i] === " ") {
+      if (line[i] === ' ') {
         output += line.slice(0, i + 1);
         // lineStart = i+1;
-        output += "\n";
+        output += '\n';
         output += line.slice(i + 1);
-        // break;
+      } else {
+        let tempSlice = line.slice(0, maxLen);
+        let lastSpace = tempSlice.lastIndexOf(' ');
+        output += line.slice(0, lastSpace + 1);
+        output += '\n';
+        output += line.slice(lastSpace + 1);
       }
     }
   }
--- wrap.spec.js
+++ wrap.spec.js
@@ -1,12 +1,20 @@
-const { expect } = require("chai");
-const wrap = require("./wrap");
+const { expect } = require('chai');
+const wrap = require('./wrap');
 
-describe("wrap", () => {
-  it("Returns empty string if empty string was provided", () => {
-    expect(wrap("", 10)).to.equal("");
+describe('wrap', () => {
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap('', 10)).to.equal('');
   });
 
-  it("New lines are not created in the middle of words", () => {
-    expect(wrap("Ceramic Galaxy", 8)).to.equal("Ceramic \nGalaxy");
+  it("New lines are created at the maxLen if it's a space", () => {
+    expect(wrap('Ceramic Galaxy', 8)).to.equal('Ceramic \nGalaxy');
+  });
+
+  it('New lines are not created in the middle of words', () => {
+    expect(wrap('The quick brown fox', 12)).to.equal('The quick \nbrown fox');
+  });
+
+  it('New lines are not created in the middle of words', () => {
+    expect(wrap('The quick brown fox jumps over the lazy dog.', 6)).to.equal('The \nquick \nbrown \nfox \njumps \nover \nthe \nlazy \ndog.');
   });
 });
